8bbf12ab7e092f3cc338814390b8c24fa56c5492
poerlz <trauernicht.dirk@gmail.com>
2015-01-15 21:53:53 +0100

    Intital commit

---
+++ formatmodel.py
@@ -0,0 +1,6 @@
+class FormatModel(object):
+	"""docstring for FormatModel"""
+	def __init__(self, arg):
+		super(FormatModel, self).__init__()
+		self.arg = arg
+		
---
+++ playlistmodel.py
@@ -0,0 +1,6 @@
+class PlayListModel(object):
+ 	"""docstring for PlayListModel"""
+ 	def __init__(self, arg):
+ 		super(PlayListModel, self).__init__()
+ 		self.arg = arg
+ 		 __init__
